5e9918df88765abe7298bdd6979748638f66cfad
Rafael4564 <rafaelgomes3900@gmail.com>
2025-11-04 14:10:54 -0300

    Add files via upload

--- menu.py
+++ menu.py
@@ -28,62 +28,7 @@ def numero_aleatorio():
         print(f"Numero aleatorio gerado entre {inicio} e {fim}: {numero}")
     input("Pressione qualquer tecla para continuar...")
 
-def calculadora():
-    while True:
-        os.system("cls")
-        print("\n=== Operações ===")
-        print("1 - Soma")
-        print("2 - Subtração")
-        print("3 - Divisão")
-        print("4 - Multiplicação")
-        print("5 - Potencia")
-        print("6 - Voltar")
-        cal = input("Escolha uma opção: ")
-        if cal == '1':
-            soma()
-        elif cal == '2':
-            sub()
-        elif cal == '3':
-            div()
-        elif cal == '4':
-            mult()
-        elif cal == '5':
-            poten()
-        elif cal == '6':
-            main()
-            break
-        else:
-            print("Opção invalida! Tente novamente")
 
-    def soma(a, b):
-        a = float(input("Digite o valor do primeiro numero: "))
-        b = float(input("Digite o valor do segundo numero: "))
-        return a + b
-    input("Pressione qualquer tecla para continuar...")
-
-    def sub(a, b):
-        a = float(input("Digite o valor do primeiro numero: "))
-        b = float(input("Digite o valor do segundo numero: "))
-        return a - b
-    input("Pressione qualquer tecla para continuar...")
-
-    def div(a, b):
-        a = float(input("Digite o valor do primeiro numero: "))
-        b = float(input("Digite o valor do segundo numero: "))
-        return a / b
-    input("Pressione qualquer tecla para continuar...")
-
-    def mult(a, b):
-        a = float(input("Digite o valor do primeiro numero: "))
-        b = float(input("Digite o valor do segundo numero: "))
-        return a * b
-    input("Pressione qualquer tecla para continuar...")
-
-    def poten(a, b):
-        a = float(input("Digite o valor do primeiro numero: "))
-        b = float(input("Digite o valor do segundo numero: "))
-        return a ** b
-    input("Pressione qualquer tecla para continuar...")
 
 def main():
     while True:
